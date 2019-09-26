6ee4150adc40750a49af5612d871cda3a7041838
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-26 11:30:16 +0000

    Update integration_test_run.sh

--- test/integration/rmq_2_isse/integration_test_run.sh
+++ test/integration/rmq_2_isse/integration_test_run.sh
@@ -5,42 +5,15 @@
 #   is located at.
 
 echo ""
-echo "Integration test:  process_msg"
+echo "Integration test"
 test/integration/rmq_2_isse/process_msg.py
-
-echo ""
-echo "Integration test:  process_msg"
 test/integration/rmq_2_isse/process_msg2.py
-
-echo ""
-echo "Integration test:  validate_create_settings"
 test/integration/rmq_2_isse/validate_create_settings.py
-
-echo ""
-echo "Integration test:  validate_create_settings"
 test/integration/rmq_2_isse/validate_create_settings2.py
-
-echo ""
-echo "Integration test:  non_proc_msg"
 test/integration/rmq_2_isse/non_proc_msg.py
-
-echo ""
-echo "Integration test:  find_files"
 test/integration/rmq_2_isse/find_files.py
-
-echo ""
-echo "Integration test:  monitor_queue"
 test/integration/rmq_2_isse/monitor_queue.py
-
-echo ""
-echo "Integration test:  run_program"
 test/integration/rmq_2_isse/run_program.py
-
-echo ""
-echo "Integration test:  main"
 test/integration/rmq_2_isse/main.py
-
-echo ""
-echo "Cleanup of RabbitMQ exchange and queues"
 test/integration/rmq_2_isse/rmq_cleanup.py
 
